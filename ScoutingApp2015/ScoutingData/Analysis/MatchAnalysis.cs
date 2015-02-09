e24048765166f0681be4bc1d196b461f105213a2
einsteinsci <einsteinsci@gmail.com>
2015-02-09 10:56:42 -0800

    quick commit again

---
+++ ScoutingApp2015/ScoutingData/Analysis/MatchAnalysis.cs
@@ -0,0 +1,37 @@
+﻿using ScoutingData.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoutingData.Analysis
+{
+	public class MatchAnalysis
+	{
+		public FrcEvent Event
+		{ get; private set; }
+
+		public Match Match
+		{ get; private set; }
+
+		public List<TeamAnalysis> TeamAnalyses
+		{ get; private set; }
+
+		#region analysis
+
+		
+
+		#endregion
+
+		public MatchAnalysis(FrcEvent e) : this(e, null, null)
+		{ }
+
+		public MatchAnalysis(FrcEvent e, Match m, List<TeamAnalysis> analyses)
+		{
+			Event = e;
+			Match = m;
+			TeamAnalyses = analyses;
+		}
+	}
+}
