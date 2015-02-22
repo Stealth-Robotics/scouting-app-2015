9e7c5f2bc3e902add4bf3c9c3f3740d01903ce7f
einsteinsci <einsteinsci@gmail.com>
2015-02-21 16:26:27 -0800

    Finished main ui layout

--- ScoutingApp2015/ScoutingApp/MainWindow.xaml.cs
+++ ScoutingApp2015/ScoutingApp/MainWindow.xaml.cs
@@ -20,9 +20,53 @@ namespace ScoutingApp
 	/// </summary>
 	public partial class MainWindow : Elysium.Controls.Window
 	{
+		public const string DEFENSE_PREFIX = "Defense: ";
+		public const string LEVEL_PREFIX = "Level: ";
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void UnprocessedLitterDownBtn_Click(object sender, RoutedEventArgs e)
+		{
+			int count = int.Parse(UnprocessedLitterCount.Text);
+			count = Math.Max(0, count - 1);
+			UnprocessedLitterCount.Text = count.ToString();
+		}
+
+		private void UnprocessedLitterUpBtn_Click(object sender, RoutedEventArgs e)
+		{
+			int count = int.Parse(UnprocessedLitterCount.Text);
+			count++;
+			UnprocessedLitterCount.Text = count.ToString();
+		}
+
+		private void DefenseSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			if (!IsLoaded)
+			{
+				return;
+			}
+
+			int val = (int)DefenseSlider.Value;
+			DefenseLbl.Content = DEFENSE_PREFIX + val.ToString();
+		}
+
+		private void ToteSetStackedToggle_Checked(object sender, RoutedEventArgs e)
+		{
+			ToteSetToggle.IsChecked = true;
+		}
+
+		private void RecyclingLevelSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			if (!IsLoaded)
+			{
+				return;
+			}
+
+			int val = (int)RecyclingLevelSlider.Value;
+			RecyclingLevelLbl.Content = DEFENSE_PREFIX + val.ToString();
 		}
 	}
 }
