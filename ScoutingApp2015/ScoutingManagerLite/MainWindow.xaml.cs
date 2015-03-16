42469655426bdb6cd444e53c5ead6cd75b5ba708
einsteinsci <einsteinsci@gmail.com>
2015-03-16 11:54:04 -0700

    Quick Commit

--- ScoutingApp2015/ScoutingManagerLite/MainWindow.xaml.cs
+++ ScoutingApp2015/ScoutingManagerLite/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Ookii.Dialogs.Wpf;
+
 namespace ScoutingManagerLite
 {
 	/// <summary>
@@ -24,6 +26,9 @@ namespace ScoutingManagerLite
 	/// </summary>
 	public partial class MainWindow : Elysium.Controls.Window
 	{
+		bool hasLoaded = false;
+		bool hasCrunched = false;
+
 		public List<TeamStatsLite> StatsList
 		{ get; private set; }
 
@@ -48,6 +53,7 @@ namespace ScoutingManagerLite
 			AllRecords = new List<List<RecordLite>>();
 			CrunchedNumbers = new Dictionary<int, RatingSet>();
 			RecordsRoot = ScoutingJson.LocalPath;
+
 			InitializeComponent();
 		}
 
@@ -75,10 +81,21 @@ namespace ScoutingManagerLite
 				foreach (FileInfo fi in recordFiles)
 				{
 					RecordLite rec = ScoutingJson.ParseLiteRecord(fi.FullName);
+					
+					if (Event != null && Teams != null)
+					{
+						rec.PostJsonLoading(Event);
+					}
+
 					recordSet.Add(rec);
 				}
 
 				AllRecords.Add(recordSet);
+			}
+
+			if (!hasCrunched)
+			{
+				CrunchNumbers();
 			}
 		}
 
@@ -90,6 +107,7 @@ namespace ScoutingManagerLite
 			}
 
 			CrunchedNumbers.Clear();
+			StatsList.Clear();
 
 			foreach (List<RecordLite> recordSet in AllRecords)
 			{
@@ -100,9 +118,131 @@ namespace ScoutingManagerLite
 
 				int teamNum = recordSet.First().TeamID;
 				TeamStatsLite stats = TeamStatsLite.MakeStats(recordSet);
+				StatsList.Add(stats);
 
 				CrunchedNumbers.Add(teamNum, stats.GetMeanRatings());
 			}
+
+			hasCrunched = true;
+		}
+
+		private void EventPathBtn_Click(object sender, RoutedEventArgs e)
+		{
+			var ofd = new Microsoft.Win32.OpenFileDialog();
+			ofd.Title = "Open FRC Event";
+			ofd.Multiselect = false;
+			ofd.Filter = "FRC Event Files (*.frc)|*.frc";
+
+			bool? result = ofd.ShowDialog();
+
+			if (result == true)
+			{
+				EventPathBox.Text = ofd.FileName;
+			}
+		}
+
+		private void TeamsPathBtn_Click(object sender, RoutedEventArgs e)
+		{
+			var ofd = new Microsoft.Win32.OpenFileDialog();
+			ofd.Title = "Open Teams List";
+			ofd.Multiselect = false;
+			ofd.Filter = "Teams List File (*.teams)|*.teams";
+
+			bool? result = ofd.ShowDialog();
+
+			if (result == true)
+			{
+				TeamsPathBox.Text = ofd.FileName;
+			}
+		}
+
+		private void RecordsPathBtn_Click(object sender, RoutedEventArgs e)
+		{
+			var fbd = new VistaFolderBrowserDialog();
+			fbd.UseDescriptionForTitle = true;
+			fbd.Description = "Select root path...";
+
+			bool? result = fbd.ShowDialog();
+
+			if (result == true)
+			{
+				RecordsPathBox.Text = fbd.SelectedPath;
+				RecordsRoot = fbd.SelectedPath;
+
+				LoadAllRecords();
+			}
+		}
+
+		private void CrunchNumbersBtn_Click(object sender, RoutedEventArgs e)
+		{
+			CrunchNumbers();
+		}
+
+		private void EventPathBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (!hasLoaded || !File.Exists(EventPathBox.Text))
+			{
+				return;
+			}
+
+			Event = ScoutingJson.ParseFrcEvent(EventPathBox.Text);
+			if (Teams != null)
+			{
+				Event.PostJsonLoading(Teams);
+			}
+		}
+
+		private void TeamsPathBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (!hasLoaded || !File.Exists(TeamsPathBox.Text))
+			{
+				return;
+			}
+
+			Teams = ScoutingJson.ParseTeamsList(TeamsPathBox.Text);
+			if (Event != null)
+			{
+				Event.PostJsonLoading(Teams);
+			}
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			hasLoaded = true;
+		}
+
+		private void InfoTeamsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			TeamStatsLite stat = InfoTeamsList.SelectedItem as TeamStatsLite;
+			if (stat == null)
+			{
+				return;
+			}
+
+			TeamNameTxt.Text = stat.Team.ToString();
+			TeamLocationTxt.Text = "From " + stat.Team.School;
+			TeamDescriptionTxt.Text = stat.Team.Description;
+
+			RatingSet vals = stat.GetMeanRatings();
+
+			AutonomousBar.Value = vals.Autonomous;
+			AutonomousTxt.Text = vals.Autonomous.ToString();
+			StackingBar.Value = vals.Stacking;
+			StackingTxt.Text = vals.Stacking.ToString();
+			CoopertitionBar.Value = vals.Coopertition;
+			CoopertitionTxt.Text = vals.Coopertition.ToString();
+			ContainersBar.Value = vals.Containers;
+			ContainersTxt.Text = vals.Containers.ToString();
+			MobilityBar.Value = vals.Mobility;
+			MobilityTxt.Text = vals.Mobility.ToString();
+			EfficiencyBar.Value = vals.Efficiency;
+			EfficiencyTxt.Text = vals.Efficiency.ToString();
+			StabilityBar.Value = vals.Stability;
+			StabilityTxt.Text = vals.Stability.ToString();
+			GripBar.Value = vals.Grip;
+			GripTxt.Text = vals.Grip.ToString();
+			HumanSkillBar.Value = vals.HumanPlayerSkill;
+			HumanSkillTxt.Text = vals.HumanPlayerSkill.ToString();
 		}
 	}
 }
